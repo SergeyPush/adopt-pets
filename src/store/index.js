9106fa61c2b6511096eb845d7c16dc19a9b4238e
Sergey Pushkovskiy <onyxpsi@ukr.net>
2020-02-25 14:38:46 +0200

    Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
+import state from "./state";
+import mutations from "./mutations";
+import actions from "./actions";
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state,
+  mutations,
+  actions,
   modules: {}
 });
---
+++ src/views/Pet.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Pet
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
